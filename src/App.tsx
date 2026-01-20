423693b026fd83919bf28cbc1ea0b9a066e05cd8
Emerson Arruda <emersonarruda753@gmail.com>
2026-01-20 19:27:29 -0300

    feat: integra estado da tarefa no componente App e atualiza CountDown e MainForm para receber props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,23 @@ import "./styles/Global.css";
 import "./styles/theme.css";
 
 import { Home } from "./pages/Home";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
+
+const initialState: TaskStateModel = {
+    task:[],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '00:00',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        worktime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15
+    }
+};
 
 export function App() {
-    return <Home/>;
+    const[state, setState] = useState(initialState);
+    return <Home state={state} setState={setState}/>;
 }
